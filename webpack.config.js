0fb8d58558f8ad8c992e50bba3d0ffb363e14d9e
Robert Kloepsch <robert.kloepsch@axeed.ch>
2024-11-09 21:02:14 +0100

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,13 +18,18 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource', // This will automatically copy the image to 'dist'
+        type: 'asset/resource',
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: './src/cloud.png', to: 'cloud.png' },
+      ],
     }),
   ],
   mode: 'development',
